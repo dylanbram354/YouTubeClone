0cd0ad5ce22c35880ad64e8c15e8789ff4c97793
dylanbram354 <dylanbram354@gmail.com>
2021-06-25 09:53:06 -0500

    Attempting to gitignore venv

--- YouTubeBackend/YouTubeBackend/YouTubeBackend/local_settings.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = '&-yt*i)fgi5xwwo!lf(oczm9_5184(r7l9^%-!z-wp%76z9c46'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'YouTubeBackend',
-    }
-}
