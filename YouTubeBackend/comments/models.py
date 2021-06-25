9ae227310dec1ae02829a148c777e63e8f0f663b
Josh <josh@Sondras-iMac.local>
2021-06-25 11:59:09 -0400

    Created Models.

--- YouTubeBackend/comments/models.py
+++ YouTubeBackend/comments/models.py
@@ -3,4 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Comment(models.model):
+    name = models.CharField(max_length=50)
     comment = models.CharField(max_length=1000)
+    date = models.DateField
+    videoId = models.CharField
+    replyToId = models.CharField(max_length=1000)
+    likes = models.PositiveIntegerField
+    dislikes = models.PositiveIntegerField
