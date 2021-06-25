b65f2851ee74e030ea48c5561d35706db663b3aa
Josh <josh@Sondras-iMac.local>
2021-06-25 14:04:25 -0400

    Added views and urls.

--- YouTubeBackend/comments/models.py
+++ YouTubeBackend/comments/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 # Create your models here.
-class Comment(models.model):
+class Comment(models.Model):
     name = models.CharField(max_length=50)
     comment = models.CharField(max_length=1000)
     date = models.DateField
     videoId = models.CharField
-    replyToId = models.CharField(max_length=1000)
+    replyToId = models.CharField(max_length=1000, null=True)
     likes = models.PositiveIntegerField
     dislikes = models.PositiveIntegerField
--- YouTubeBackend/comments/views.py
+++ YouTubeBackend/comments/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .models import Comment
+from .serializers import CommentSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+class CommentList(APIView):
+
+    def get(self, request, video_id):
+        comments = Comment.objects.all(videoId=video_id)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
