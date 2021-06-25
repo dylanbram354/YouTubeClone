2d19a2c25e8c9a5b17c0698b71984d234abab817
Josh <josh@Sondras-iMac.local>
2021-06-25 10:30:19 -0400

    Created Pycharm backend for DB.

---
+++ YouTubeBackend/YouTubeBackend/YouTubeBackend/local_settings.py
@@ -0,0 +1,8 @@
+SECRET_KEY = '&-yt*i)fgi5xwwo!lf(oczm9_5184(r7l9^%-!z-wp%76z9c46'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
